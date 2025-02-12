6959a69fc9e7467168effa4433b21c220a83f668
aymensellaouti <aymen.sellaouti@gmail.com>
2025-02-12 13:31:48 +0100

    detailsCv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,14 @@ import { CvComponent } from './cv/cv/cv.component';
 import { MiniWordComponent } from './directives/mini-word/mini-word.component';
 import { ColorComponent } from './components/color/color.component';
 import { SecondComponent } from './components/second/second.component';
+import { APP_ROUTES } from './config/app-routes.config';
+import { DetailsCvComponent } from './cv/details-cv/details-cv.component';
 // cv
 const routes: Routes = [
   { path: '', component: FirstComponent},
   { path: 'todo', component: TodoComponent},
-  { path: 'cv', component: CvComponent},
+  { path: APP_ROUTES.cv, component: CvComponent},
+  { path: `${APP_ROUTES.cv}/:id`, component: DetailsCvComponent},
   { path: 'word', component: MiniWordComponent},
   { path: 'color', component: ColorComponent},
   { path: ':quelqueChose', component: SecondComponent},
--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -1,5 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { Cv } from "../model/cv.model";
+import { ActivatedRoute, Router } from "@angular/router";
+import { CvService } from "../services/cv.service";
+import { APP_ROUTES } from "src/app/config/app-routes.config";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-details-cv',
@@ -7,7 +11,25 @@ import { Cv } from "../model/cv.model";
   styleUrls: ['./details-cv.component.css'],
 })
 export class DetailsCvComponent {
-  cv: Cv | null = null;
+  acr = inject(ActivatedRoute);
+  router = inject(Router);
+  cvService = inject(CvService);
+  toastr = inject(ToastrService);
+  private id = this.acr.snapshot.params['id'];
+  cv: Cv | null = this.cvService.findCvById(this.id);
+  constructor() {
+    if (!this.cv) {
+      this.router.navigate([APP_ROUTES.cv]);
+    }
+  }
+  delete() {
+    if (this.cv) {
+      if(this.cvService.deleteCv(this.cv)) {
+        this.router.navigate([APP_ROUTES.cv]);
+      } else {
+        this.toastr.error(`Erreur lors de la suppression !! `)
+      }
 
-  constructor() {}
+    }
+  }
 }
